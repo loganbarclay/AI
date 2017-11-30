a79e2e632e2569066abc56625947a5be9b028409
Wesley Gruenberg <wesleygruenberg@u.boisestate.edu>
2017-11-30 12:41:32 -0700

    changing sentence

--- JavaProver/src/Sentence.java
+++ JavaProver/src/Sentence.java
@@ -1,207 +1,185 @@
-import java.util.Arrays;
-import java.util.HashSet;
+
 import java.util.PriorityQueue;
 import java.util.Random;
 
-
 public class Sentence implements Comparable<Sentence> {
 
-    private PriorityQueue<Predicate> predicateQueue;
-    private Sentence firstParent;
-    private Sentence secondParent;
-    private String substitution = null;
-    private int score;
-    private boolean heuristics;
+	private PriorityQueue<Predicate> predicateQueue;
+	private Sentence firstParent;
+	private Sentence secondParent;
+	private String substitution = null;
+	private int score;
+	private boolean heuristics;
+	private final int PREDICATE_WEIGHT = 5;
+	private final int PARAMETER_WEIGHT = 1;
 
-    public Sentence(PriorityQueue<Predicate> preds, boolean heuristics)
-    {
-    	this.heuristics = heuristics;
-        this.predicateQueue = preds;
-        score(heuristics);
+	public Sentence(PriorityQueue<Predicate> preds, boolean heuristics) {
+		this.heuristics = heuristics;
+		this.predicateQueue = preds;
+		score(heuristics);
 
-    }
-    
-    public Sentence(PriorityQueue<Predicate> preds, Sentence p1, Sentence p2, boolean heuristics)
-    {
-    	this.heuristics = heuristics;
-        this.predicateQueue = preds;
-        this.firstParent = p1;
-        this.secondParent = p2;
-        score(heuristics);
-        
-    }
-    
-    public Sentence(PriorityQueue<Predicate> preds, Sentence p1, Sentence p2, String substitution, boolean heuristics)
-    {
-    	this.heuristics = heuristics;
-        this.predicateQueue = preds;
-        this.firstParent = p1;
-        this.secondParent = p2;
-        this.substitution = substitution;
-        score(heuristics);
-    }
+	}
 
-    @Override
-    public String toString()
-    {
-    	String retVal = "";
-       // Predicate[] predArray = preds.toArray(new Predicate[0]);
-       // Arrays.sort(predArray);
-        
-        for (Predicate predicate : predicateQueue)
-        {
-        	retVal += predicate.toString();
-        	//System.out.println(retVal);
-        }
-        retVal += "\n";
-     
-        return retVal;
-    }
+	public Sentence(PriorityQueue<Predicate> preds, Sentence firstParent, Sentence secondParent, boolean heuristics) {
+		this.heuristics = heuristics;
+		this.predicateQueue = preds;
+		this.firstParent = firstParent;
+		this.secondParent = secondParent;
+		score(heuristics);
 
-    @Override
-    public int hashCode()
-    {
-        return toString().hashCode();
-    }
+	}
 
-    @Override
-    public boolean equals(Object o)
-    {
-        Sentence s = (Sentence) o;
-        return this.hashCode() == s.hashCode();
-    }
+	public Sentence(PriorityQueue<Predicate> preds, Sentence firstParent, Sentence secondParent, String substitution, boolean heuristics) {
+		this.heuristics = heuristics;
+		this.predicateQueue = preds;
+		this.firstParent = firstParent;
+		this.secondParent = secondParent;
+		this.substitution = substitution;
+		score(heuristics);
+	}
+	
+	private void score(boolean heuristics) {
+		if (heuristics) {
+			heuristicScore();
 
-    @Override
-    public int compareTo(Sentence that)
-    {
-        //return lower score for priority queue
-        return Integer.compare(that.score, this.score);
-    }
+		} else {
+			randomScore();
 
-    public PriorityQueue<Predicate> getPreds()
-    {
-        return predicateQueue;
-    }
-    
-    public boolean isGoal()
-    {
-        return predicateQueue.isEmpty() && firstParent != null && secondParent != null;
-    }
+		}
+	}
 
-    private void score(boolean heuristics){
-        if(heuristics){
-        	heuristicScore();
-        	
-        }
-        else{
-        	randomScore();
-        	
-        }
-    }
-    public void heuristicScore()
-    {
-        this.score = 0;
-        this.score -= 5*predicateQueue.size();
-        
-        for (Predicate predicate : predicateQueue)
-        {
-        	this.score -= predicate.getParams().size();
-            
-        }
+	public void heuristicScore() {
+		this.score = 0;
+		for (Predicate predicate : predicateQueue) {
+			this.score -= PARAMETER_WEIGHT * predicate.getParams().size();
+		}
+		this.score -= PREDICATE_WEIGHT * predicateQueue.size();
 
-    }
-    
-    public void randomScore(){
-    	Random rand = new Random();
-    	this.score = rand.nextInt(20);
-    }
-    
+	}
 
-    public int getScore(){
-    	return this.score;
-    }
-    
-    public Sentence resolve(Sentence that)
-    {
-        for (Predicate p1 : this.predicateQueue)
-        {
-            for (Predicate p2 : that.getPreds())
-            {
-                if (p1.isUnifiable(p2))
-                {
-                    String sub = p1.unify(p2);
-                    if (sub != null && sub.equals(""))
-                    {
-                        Sentence thisRemoved = this.remove(p1).remove(p2);
-                        Sentence thatRemoved = that.remove(p1).remove(p2);
-                        return thisRemoved.or(thatRemoved, this, that);
-                    }
-                    else if (sub != null)
-                    {
-                        Sentence thisRemoved = this.remove(p1).remove(p2);
-                        Sentence thatRemoved = that.remove(p1).remove(p2);
-                        Sentence thisSubbed = thisRemoved.substitute(sub);
-                        Sentence thatSubbed = thatRemoved.substitute(sub);
-                        return thisSubbed.or(thatSubbed, this, that);
-                    }
-                    else
-                    {
-                        continue;
-                    }
-                }
-            }
-        }
-        return null;
-    }
+	public void randomScore() {
+		Random rand = new Random();
+		this.score = rand.nextInt(20);
+	}
 
-    private Sentence substitute(String sub)
-    {
-        PriorityQueue<Predicate> newPreds = new PriorityQueue<>();
-        for (Predicate predicate : this.getPreds())
-        {
-            newPreds.add(predicate.substitute(sub));
-        }
-        return new Sentence(newPreds, this.firstParent, this.secondParent, sub, this.heuristics);
-    }
+	public int getScore() {
+		return this.score;
+	}
 
-    private Sentence or(Sentence that, Sentence thisP, Sentence thatP)
-    {
-        PriorityQueue<Predicate> combinedPreds = new PriorityQueue<>();
-        for (Predicate predicate : this.getPreds())
-        {
-            combinedPreds.add(predicate);
-        }
-        for (Predicate predicate : that.getPreds())
-        {
-            combinedPreds.add(predicate);
-        }
-        return new Sentence(combinedPreds, thisP, thatP, this.substitution, this.heuristics);
-    }
-    
-    private Sentence remove(Predicate p1)
-    {
-        Predicate p1Toggled = p1.toggleNegation();
-        PriorityQueue<Predicate> toRemove = new PriorityQueue<>();
-      
-		PriorityQueue<Predicate> clone = new PriorityQueue<>(predicateQueue);
-        for (Predicate predicate : clone)
-        {
-            if (predicate.equals(p1) || predicate.equals(p1Toggled))
-            {
-                toRemove.add(predicate);
-            }
-        }
-        clone.removeAll(toRemove);
-        return new Sentence(clone, this.firstParent, this.secondParent, this.heuristics);
-    }
-    
-    public String getParents(String path)
-    {
-        if (this.firstParent != null && this.secondParent != null)
-        {   String me = this.toString().trim().equals("") ? "goal " : this.toString().replace("\n", "") ;
-            String derivedFrom = path + "\nunify(" + this.firstParent.toString().replace("\n", "") + ", " + this.secondParent.toString().replace("\n", "") + ") => " + me;
-            return this.firstParent.getParents("") + this.secondParent.getParents("") + derivedFrom + (this.substitution == null ? "Negations cancel" : "Substitution:" + this.substitution.replaceAll("/", "="));
-        }
-        return path;
-    }
+
+	public PriorityQueue<Predicate> getPreds() {
+		return predicateQueue;
+	}
+
+	public boolean isGoal() {
+		if (firstParent != null && secondParent != null && predicateQueue.isEmpty()) {
+			return true;
+		}
+		return false;
+	}
+
+
+
+	public Sentence resolve(Sentence that) {
+		Sentence retSentence = null;
+		for (Predicate pred1 : this.predicateQueue) {
+			for (Predicate pred2 : that.getPreds()) {
+				if (pred1.isUnifiable(pred2)) {
+					String sub = pred1.unify(pred2);
+					if (sub != null && sub.equals("")) {
+						Sentence removeThis = this.remove(pred1).remove(pred2);
+						Sentence removedFromThat = that.remove(pred1).remove(pred2);
+						
+						retSentence = removeThis.folOR(removedFromThat, this, that);
+					} else if (sub != null) {
+						Sentence thisRemoved = this.remove(pred1).remove(pred2);
+						Sentence thatRemoved = that.remove(pred1).remove(pred2);
+						Sentence thisSubbed = thisRemoved.substitution(sub);
+						Sentence thatSubbed = thatRemoved.substitution(sub);
+						
+						retSentence = thisSubbed.folOR(thatSubbed, this, that);
+					} else {
+						continue;
+					}
+				}
+			}
+		}
+		return retSentence;
+	}
+
+	private Sentence substitution(String substStr) {
+		PriorityQueue<Predicate> newPreds = new PriorityQueue<>();
+		for (Predicate predicate : this.getPreds()) {
+			newPreds.add(predicate.substitute(substStr));
+		}
+		return new Sentence(newPreds, this.firstParent, this.secondParent, substStr, this.heuristics);
+	}
+
+	private Sentence folOR(Sentence that, Sentence thisP, Sentence thatP) {
+		PriorityQueue<Predicate> combinedPreds = new PriorityQueue<>();
+		for (Predicate predicate : this.getPreds()) {
+			combinedPreds.add(predicate);
+		}
+		for (Predicate predicate : that.getPreds()) {
+			combinedPreds.add(predicate);
+		}
+		return new Sentence(combinedPreds, thisP, thatP, this.substitution, this.heuristics);
+	}
+
+	private Sentence remove(Predicate pred) {
+		Sentence retSentence;
+		Predicate negatedPred = pred.toggleNegation();
+
+		PriorityQueue<Predicate> predQueueCopy = new PriorityQueue<>(predicateQueue);
+		for (Predicate predicate : predicateQueue) {
+			if (predicate.equals(pred) || predicate.equals(negatedPred)) {
+				predQueueCopy.remove(predicate);
+			}
+		}
+
+		retSentence = new Sentence(predQueueCopy, this.firstParent, this.secondParent, this.heuristics);
+		return retSentence;
+	}
+
+	public String getParents(String path) {
+		if (this.firstParent != null && this.secondParent != null) {
+			String me = this.toString().trim().equals("") ? "goal " : this.toString().replace("\n", "");
+			String derivedFrom = path + "\nunify(" + this.firstParent.toString().replace("\n", "") + ", "
+					+ this.secondParent.toString().replace("\n", "") + ") => " + me;
+			return this.firstParent.getParents("") + this.secondParent.getParents("") + derivedFrom
+					+ (this.substitution == null ? "Negations cancel"
+							: "Substitution:" + this.substitution.replaceAll("/", "="));
+		}
+		return path;
+	}
+	
+	@Override
+	public String toString() {
+		String retVal = "";
+
+		for (Predicate predicate : predicateQueue) {
+			retVal += predicate.toString();
+		}
+		retVal += "\n";
+
+		return retVal;
+	}
+
+	@Override
+	public int hashCode() {
+		return toString().hashCode();
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		Sentence s = (Sentence) o;
+		return this.hashCode() == s.hashCode();
+	}
+
+	@Override
+	public int compareTo(Sentence that) {
+		// return lower score for priority queue
+		return Integer.compare(that.score, this.score);
+	}
+
 }
